c1744ae54787f5c21b2d1a361fc64816e30620dd
Kaylee210 <kaylee.nov.210@gmail.com>
2019-03-09 06:25:28 +0900

    Done cording for test

--- BuildARecommendationSystem/week2/StepOneStarterProgram/MovieRunnerAverage.java
+++ BuildARecommendationSystem/week2/StepOneStarterProgram/MovieRunnerAverage.java
@@ -84,5 +84,71 @@ public class MovieRunnerAverage {
         }
     }
     
+    /*
+     * Method for test
+     */
+    public void test(){
+        
+        // Create SecondRatings object
+        String mfilename = "data/ratedmoviesfull.csv";
+        String rfilename = "data/ratings.csv";
+        SecondRatings secondRating = new SecondRatings(mfilename,  rfilename);
+        
+        // Get movie id for especially title
+        String title1 = "The Maze Runner";
+        String id1 = secondRating.getID(title1);
+        // Get average
+        ArrayList<Rating> rating1 = secondRating.getAverageRatings(1);
+        for(Rating r : rating1){
+            String tmpID = r.getItem ();
+            if(id1.equals(tmpID)){
+                System.out.println(title1 + "'s rating average is " + r.getValue());
+            }
+        }
+        
+        // Get movie id for especially title
+        String title2 = "Moneyball";
+        String id2 = secondRating.getID(title2);
+        // Get average
+        ArrayList<Rating> rating2 = secondRating.getAverageRatings(1);
+        for(Rating r : rating2){
+            String tmpID = r.getItem ();
+            if(id2.equals(tmpID)){
+                System.out.println(title2 + "'s rating average is " + r.getValue());
+            }
+        }
+        
+        // Get movie id for especially title
+        String title3 = "Vacation";
+        String id3 = secondRating.getID(title3);
+        // Get average
+        ArrayList<Rating> rating3 = secondRating.getAverageRatings(1);
+        for(Rating r : rating3){
+            String tmpID = r.getItem ();
+            if(id3.equals(tmpID)){
+                System.out.println(title3 + "'s rating average is " + r.getValue());
+            }
+        }
+        
+        // Get rating count
+        ArrayList<Rating> rating4 = secondRating.getAverageRatings(50);
+        System.out.println("The movies having 50 or more is " + rating4.size());
+        
+        // Get rating data
+        ArrayList<Rating> rating5 = secondRating.getAverageRatings(20);
+        sortByValue(rating5);
+        String id5 = rating5.get(0).getItem();
+        String title5 = secondRating.getTitle(id5);
+        System.out.println("Lowest movie is " + title5);
+        
+        // Get rating data
+        ArrayList<Rating> rating6 = secondRating.getAverageRatings(12);
+        sortByValue(rating6);
+        String id6 = rating6.get(0).getItem();
+        String title6 = secondRating.getTitle(id6);
+        System.out.println("Lowest movie is " + title6);
+        
+        
+    }
     
 }
